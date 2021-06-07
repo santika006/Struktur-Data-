7a99ab94c20aa451b6283be0578ebd1119a2eab1
santika006 <72698209+santika006@users.noreply.github.com>
2021-06-07 18:33:23 +0700

    Update Praktik 6 no.2.cpp

--- Praktik 6 no.2.cpp
+++ Praktik 6 no.2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+using namespace std;
 int main(int argc, char** argv) {
 	int i;
 int n
