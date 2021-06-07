7338882492e772cb6a8811203688a2654eb49106
santika006 <72698209+santika006@users.noreply.github.com>
2021-06-07 18:26:41 +0700

    Add files via upload

---
+++ Praktik 6 no.2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int i;
+int n
+{
+    cout<<"Masukkan angka :" ;
+    cin>>n;
+     for (i=0; i<n; i++)
+    {
+        cout<<"Rekursi 0 Ke N : "<<i;
+        cout<<endl;
+    }
+    return n;
+}
+
+int main ()
+{
+    int n;
+    int i;
+    cout<<angka(i);
+}
+
+	return 0;
+}
