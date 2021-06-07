ecfc2a17c814cc310e536fc0f304adefc611e965
santika006 <72698209+santika006@users.noreply.github.com>
2021-06-07 18:11:03 +0700

    Add files via upload

---
+++ Praktik 3 no.1.cpp
@@ -0,0 +1,110 @@
+//SANTIKA LANA HAYATI
+//20051397006
+//2020B_D4MI
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(int argc, char** argv) {
+	int cqueue[5];
+int front = -1, rear = -1, n=5;
+
+void insertCQ(int val) {
+	cout<<"SANTIKA LANA HAYATI \n";
+	cout<<"20051397006 \n";
+	cout<<"2020B \n";
+   if ((front == 0 && rear == n-1) || (front == rear+1)) {
+      cout<<"Queue Overflow \n";
+      return;
+   }
+   if (front == -1) {
+      front = 0;
+      rear = 0;
+   } else {
+      if (rear == n - 1)
+      rear = 0;
+      else
+      rear = rear + 1;
+   }
+   cqueue[rear] = val ;
+}
+void deleteCQ() {
+   if (front == -1) {
+      cout<<"Queue Underflow\n";
+      return ;
+   }
+   cout<<"Element deleted from queue is : "<<cqueue[front]<<endl;
+
+   if (front == rear) {
+      front = -1;
+      rear = -1;
+   } else {
+      if (front == n - 1)
+      front = 0;
+      else
+      front = front + 1;
+   }
+}
+void displayCQ() {
+   int f = front, r = rear;
+   if (front == -1) {
+      cout<<"Queue is empty"<<endl;
+      return;
+   }
+   cout<<"Queue elements are :\n";
+   if (f <= r) {
+      while (f <= r){
+         cout<<cqueue[f]<<" ";
+         f++;
+      }
+   } else {
+      while (f <= n - 1) {
+         cout<<cqueue[f]<<" ";
+         f++;
+      }
+      f = 0;
+      while (f <= r) {
+         cout<<cqueue[f]<<" ";
+         f++;
+      }
+   }
+   cout<<endl;
+}
+int main() {
+
+   int ch, val;
+   cout<<"1)Insert\n";
+   cout<<"2)Delete\n";
+   cout<<"3)Display\n";
+   cout<<"4)Exit\n";
+   do {
+      cout<<"Enter choice : "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1:
+         cout<<"Input for insertion: "<<endl;
+         cin>>val;
+         insertCQ(val);
+         break;
+
+         case 2:
+         deleteCQ();
+         break;
+
+         case 3:
+         displayCQ();
+         break;
+
+         case 4:
+         cout<<"Exit\n";
+         break;
+         default: cout<<"Incorrect!\n";
+      }
+   } while(ch != 4);
+   return 0;
+}
