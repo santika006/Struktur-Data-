b0e310a91137a4c4c420516270cd06191a24e244
santika006 <72698209+santika006@users.noreply.github.com>
2021-06-07 18:22:55 +0700

    Add files via upload

---
+++ Praktik 6 no.1.cpp
@@ -0,0 +1,31 @@
+//SANTIKA LANA HAYATI
+//20051397006
+//2020B_D4MI
+
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int i;
+int n
+{
+    cout<<"Masukkan Huruf :" ;
+    cin>>n;
+     for (i=n; i<0; i++)
+    {
+        cout<<"Rekursi N Ke 0 : "<<i;
+        cout<<endl;
+    }
+    return n;
+}
+
+int main ()
+{
+    int n;
+    int i;
+    cout<<angka(i);
+}
+
+	return 0;
+}
