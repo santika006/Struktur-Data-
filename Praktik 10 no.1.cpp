8bad2be8e38dd70189c138120df7eaa5cc060534
santika006 <72698209+santika006@users.noreply.github.com>
2021-06-07 19:14:36 +0700

    Add files via upload

---
+++ Praktik 10 no.1.cpp
@@ -0,0 +1,32 @@
+//SANTIKA LANA HAYAYTI
+//20051397006
+//2020B_D4MI
+
+#include <iostream>
+#include <conio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int main()
+{
+for(int i=0;i<20;i++){
+ cout<<A[i]<<" ";
+}
+cout<<"Santika Lana Hayati\n";
+cout<<"20051397006\n";
+cout<<"MI_2020B\n";
+cout<<"\nData yang ingin dicari : ";
+cin>>c;
+i=0;
+posisi=0;
+while(i<19 && A[i]!=c){
+ i++;
+}
+if (A[i]!=c){
+ cout<<"Maaf data yang dicari tidak ada";
+}else if(posisi=i+1)
+   cout<<"ditemukan pada posisi ke "<<posisi;
+getch();
+}
+	return 0;
+}
